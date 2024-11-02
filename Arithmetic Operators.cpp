baa29fddfdf50370d096e5b7db0c73b3b7072233
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-03 01:01:35 +0530

    Add files via upload

---
+++ Arithmetic Operators.cpp
@@ -0,0 +1,21 @@
+//Program to print the arithmetic Operators in C++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 49, b = 13;
+
+    cout << "a + b = " << (a + b) << endl;
+
+    cout << "a - b = " << (a - b) << endl;
+
+    cout << "a * b = " << (a * b) << endl;
+
+    cout << "a / b = " << (a / b) << endl;
+
+    cout << "a % b = " << (a % b) << endl;
+
+    return 0;
+}
